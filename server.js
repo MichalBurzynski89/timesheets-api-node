c79876ba7676c4eb1acf2171009d8babe71637b4
Michal Burzynski (PGS Software) <mburzynski@pgs-soft.com>
2022-04-26 17:43:58 +0200

    Throw an error, if one of the AUTH0 environment variables is missing, install colors

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+/* eslint-disable no-unused-vars */
+
 const express = require('express');
+const colors = require('colors');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config(); // eslint-disable-line no-unused-vars
+const dotenv = require('dotenv').config();
 const port = process.env.PORT || 8080;
 
 const app = express();
+
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+  throw new Error(
+    'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file'.red.underline
+  );
+}
 
 // Enable CORS
 app.use(cors());
